7d217708b524644e8da3dfd2ce6b98c7972cad04
Ivan Hernandez <Ivan_Hernandez@epam.com>
2016-11-07 10:56:30 -0600

    Initial commit

---
+++ desktop/src/com/gdlactivity/libgdxdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.gdlactivity.libgdxdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdlactivity.libgdxdemo.GDLActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 400;
+		config.height = 800;
+		new LwjglApplication(new GDLActivity(), config);
+	}
+}
---
+++ html/src/com/gdlactivity/libgdxdemo/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.gdlactivity.libgdxdemo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.gdlactivity.libgdxdemo.GDLActivity;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GDLActivity();
+        }
+}
